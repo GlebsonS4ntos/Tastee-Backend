69e694c2a1d32da00c73fd81ee749c19a0d9ef18
Glebson Santos <glebsonsantos@outlook.pt>
2025-07-29 20:01:44 -0300

    init: create repository

---
+++ src/main/java/com/Glebson/Tastee/TasteeApplication.java
@@ -0,0 +1,13 @@
+package com.Glebson.Tastee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Glebson/Tastee/TasteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Glebson.Tastee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
