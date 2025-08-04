3dcc5ccd534646f39f41d45ccfb39e192961b7a4
Glebson Santos <glebsonsantos@outlook.pt>
2025-08-04 08:39:34 -0300

    feat: create repositories

---
+++ src/main/java/com/Glebson/Tastee/Repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.Glebson.Tastee.Repositories;
+
+import com.Glebson.Tastee.Domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
