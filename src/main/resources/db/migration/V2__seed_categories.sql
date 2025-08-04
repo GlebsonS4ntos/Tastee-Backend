c9bf91ef3af32f2e31966f6407e8a3a2400f49a9
Glebson Santos <glebsonsantos@outlook.pt>
2025-08-04 08:38:10 -0300

    feat: seed migration

---
+++ src/main/resources/db/migration/V2__seed_categories.sql
@@ -0,0 +1,11 @@
+INSERT INTO category (id, name) VALUES
+(1, 'Salgados'),
+(2, 'Doces'),
+(3, 'Peixe'),
+(4, 'Almoço'),
+(5, 'Jantar'),
+(6, 'Café da Manhã'),
+(7, 'Café da Tarde'),
+(8, 'Carne'),
+(9, 'Massa'),
+(10, 'Saudável');
