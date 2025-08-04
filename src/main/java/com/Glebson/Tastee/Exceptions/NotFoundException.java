c3285ad537af0fe4f0e0809810eff42f2cfba356
Glebson Santos <glebsonsantos@outlook.pt>
2025-08-04 10:48:58 -0300

    feat: Not Found Exception

---
+++ src/main/java/com/Glebson/Tastee/Exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.Glebson.Tastee.Exceptions;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
