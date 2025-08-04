e5105477a14c45d5a7e8adc573d8ef2b0bf3a034
Glebson Santos <glebsonsantos@outlook.pt>
2025-08-04 16:46:05 -0300

    feat: Image Entity

---
+++ src/main/java/com/Glebson/Tastee/Domain/Image.java
@@ -0,0 +1,68 @@
+package com.Glebson.Tastee.Domain;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity()
+public class Image implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String urlImg;
+    private String imgId;
+    @OneToOne(mappedBy = "image")
+    private Post post;
+
+    public Image() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrlImg() {
+        return urlImg;
+    }
+
+    public void setUrlImg(String urlImg) {
+        this.urlImg = urlImg;
+    }
+
+    public String getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(String imgId) {
+        this.imgId = imgId;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Image image)) return false;
+        return Objects.equals(getId(), image.getId()) && Objects.equals(getUrlImg(), image.getUrlImg());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUrlImg());
+    }
+}
