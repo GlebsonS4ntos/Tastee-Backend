e6860f981eb8cf0f333cd435b48c2d75184130d1
Glebson Santos <glebsonsantos@outlook.pt>
2025-08-08 06:01:10 -0300

    fix: remove post fk

--- src/main/resources/db/migration/V3__add_img_entity.sql
+++ src/main/resources/db/migration/V3__add_img_entity.sql
@@ -5,9 +5,3 @@ CREATE TABLE image (
     post_id BIGINT UNIQUE,
     CONSTRAINT fk_image_post FOREIGN KEY (post_id) REFERENCES post(id)
 );
-
-ALTER TABLE post
-ADD COLUMN image_id BIGINT;
-
-ALTER TABLE post
-ADD CONSTRAINT fk_post_image FOREIGN KEY (image_id) REFERENCES image(id);
